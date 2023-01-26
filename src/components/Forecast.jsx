7ce6021f9241f194c827c4d7dc71a25bc6c1dbd2
chandlerkeyes <chandlerkeyes@yahoo.com>
2023-01-25 22:16:58 -0600

    Added API call that gets the current weather based on the lat & long coordinates. Added a Get Weather button that triggers this API call.

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -5,22 +5,32 @@ import React, { useEffect, useState } from "react";
 const Forecast = () => {
     const [coordinates, setCoordinates] = useState({ lat: null, long: null });
     const getCoordinates = async () => {
-        return await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=Houston,TX,US&limit=1&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`);
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_OPEN_WEATHER_API_HOST}/geo/1.0/direct?q=Houston,TX,US&limit=1&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`);
+            setCoordinates({ lat: response.data[0].lat, long: response.data[0].lon });
+        } catch (e) {
+            console.error("Could not fetch coordinates from OpenWeatherMap API: ", e);
+        }
+    };
+
+    const get4DayForecast = async () => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_OPEN_WEATHER_API_HOST}/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.long}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`);
+            console.log("WEATHER: ", response.data);
+        } catch (e) {
+            console.error("Could not fetch 4 Day Forecast from OpenWeatherMap API: ", e);
+        }
     };
 
     useEffect(() => {
-        getCoordinates().then((response) => {
-            const coordinateData = response.data[0];
-            setCoordinates({ lat: coordinateData.lat, long: coordinateData.lon });
-        }).catch((e) => {
-            console.log("Could not fetch data from OpenWeatherMap API: ", e);
-        });
+        getCoordinates();
     }, []);
 
     return <div>
         <h2>4-Day Forecast</h2>
         <div>Lat: {coordinates.lat}</div>
         <div>Long: {coordinates.long}</div>
+        <button onClick={get4DayForecast}>Get Weather</button>
     </div>;
 }
 
